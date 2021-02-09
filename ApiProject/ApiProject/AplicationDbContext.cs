7a48e1f67649ef1ac05d7e2f05ad1b0e7655acf5
JesusLinares01 <alberto.salinas@asesoriait.com>
2021-02-09 00:43:33 -0600

    "Creation model, DbContext configuration and data base migration"

---
+++ ApiProject/ApiProject/AplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiProject
+{
+    public class AplicationDbContext: DbContext
+    {
+        public AplicationDbContext(DbContextOptions<AplicationDbContext> options): base(options) { 
+            
+        }
+
+        public DbSet<CreditCard> CreditCard { get; set; }
+    }
+}
